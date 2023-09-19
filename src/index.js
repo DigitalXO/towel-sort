c060730f75c7e352a99eec8cd0ed7e69aa714671
DigitalXO <sultanofsicness@icloud.com>
2023-09-19 22:09:39 +0300

    init: auto test done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+
+  return matrix.reduce((acc, row, index) => {
+    if (index % 2 === 0) {
+      // Если индекс строки четный, просто объединяем с текущим аккумулятором.
+      return acc.concat(row);
+    } else {
+      // Если индекс строки нечетный, объединяем с текущим аккумулятором, но в обратном порядке.
+      return acc.concat(row.reverse());
+    }
+  }, []);
+};
